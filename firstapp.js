e947159eb08a32bd20ea42278ab9c9f63c0fa9a2
Irm <idrishan1996@gmail.com>
2023-06-28 10:26:19 +0530

    creating a file with node

--- firstapp.js
+++ firstapp.js
@@ -1,2 +1,17 @@
-console.log('Hi')
+const fs = require('fs');
+
+const data = {
+    "userId": "0001",
+    "userName": "DenCap",
+    "firstName": "Denver",
+    "lastName": "Capsi",
+    "address": {
+        "street": "Dubai kuruku Sandhu",
+        "road": "Dubai Main Road",
+        "City": "Dubai"
+    }
+
+}
+fs.writeFileSync('hello.json', JSON.stringify(data))
+
 // to run -> node firstapp.js
