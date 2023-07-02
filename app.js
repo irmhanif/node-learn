c1a0cbfd45f8957e48846b01642c7c73b9cd0495
Irm <idrishan1996@gmail.com>
2023-07-02 20:34:05 +0530

    Blocking and non blocking code

--- app.js
+++ app.js
@@ -11,19 +11,20 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
-    if(url === '/message' && method === 'POST') {
+    if (url === '/message' && method === 'POST') {
         const body = [];
-        req.on('data', (chunk)=> {
+        req.on('data', (chunk) => {
             body.push(chunk);
         });
-        req.on('end', ()=> {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
         //res.writeHead(302, {})
     }
     res.setHeader('Content-Type', 'text/html');
