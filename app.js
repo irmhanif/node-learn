4ad7ad083f97adb1d3497642f4d15db06dc12249
Irm <idrishan1996@gmail.com>
2023-07-04 10:02:15 +0530

    Installing express

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+//const routes = require('./routes');//
+
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
