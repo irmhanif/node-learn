b9d39874c93e465811218eb4ad27e8f84efb0100
Irm <idrishan1996@gmail.com>
2023-07-05 20:47:13 +0530

    Sharing data and installing template engines

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,18 @@ const path = require('path');
 const router = express.Router();
 const rootDir = require('../util/path')
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     //res.send(`<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`)
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
-router.post('/add-product', (req, res)=> {
-    console.log(req.body)
-    
+router.post('/add-product', (req, res) => {
+    products.push({ title: req.body.title })
+
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products
