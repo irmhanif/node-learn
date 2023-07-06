48641f5bbe616a762458e94fb6b042dc4bf19e5f
Irm <idrishan1996@gmail.com>
2023-07-06 18:38:28 +0530

    Enhanced the app

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,19 @@
+const path = require('path');
+
 const express = require('express');
 
+const shopController = require('../controllers/shop');
 
-const productHandler = require('../controllers/products')
 const router = express.Router();
 
-router.get('/', productHandler.getProduct);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
